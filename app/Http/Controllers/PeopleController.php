02a167c8c9c0302e5fcde649894268c40a81a682
quoc le <dinhquoc0103@gmail.com>
2022-10-04 16:33:25 +0700

    fix sql

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -9,7 +9,7 @@ class PeopleController extends Controller
 {
     public function index()
     {
-        $peoples = DB::table("test")->all();
+        $peoples = DB::table("test")->select()->get();
         return view("index", [
             "peoples" => $peoples
         ]);
